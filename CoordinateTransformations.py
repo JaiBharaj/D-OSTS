8a2782941dfdee8574c28dc1c286c724343f9705
JaiBharaj <85242765+JaiBharaj@users.noreply.github.com>
2025-05-02 11:46:34 +0100

    Add files via upload

---
+++ CoordinateTransformations.py
@@ -0,0 +1,90 @@
+import numpy as np
+
+# Placeholder Atmospheric Density Model
+def atmospheric_density():
+    return 1
+
+# Placeholder Initial Conditions Class
+class InitialConditions:
+
+    dragCoeff = 2
+    crossSec = 20
+    satMass = 20
+    initSatAlt = 600000
+    earthMass = 6E+24
+    earthRadius = 6.37E+6
+    gravConstant = 6.67E-11
+
+class PolarAccelerations:
+
+    # Polar Velocity Magnitude
+    @staticmethod
+    def comb_velocity(rad, radVel, angVel):
+        return np.sqrt(radVel**2 + (rad * angVel)**2)
+
+    # First part of Drag term: consistent for Spherical coordinates
+    @staticmethod
+    def drag_start(C_d, A, m):
+        return atmospheric_density * C_d * A / (2 * m)
+
+    # Polar Acceleration functions to be passed into RK45
+    @staticmethod
+    def accelerations(u1, u2, u3, u4):
+
+        '''
+        :param u1: Radial Position
+        :param u2: Radial Velocity
+        :param u3: Angular Position
+        :param u4: Angular Velocity
+
+        :return: [Radial Velocity, Radial Acceleration, Angular Velocity, Angular Acceleration]
+        '''
+
+        dt_start = PolarAccelerations.drag_start(C_d=InitialConditions.dragCoeff,
+                                                 A=InitialConditions.crossSec,
+                                                 m=InitialConditions.satMass)
+
+        vel_mag = PolarAccelerations.comb_velocity(u1, u2, u4)
+
+        u1_dot = u2
+        u2_dot = u1 * u4**2 - InitialConditions.gravConstant * InitialConditions.earthMass / u1**2 - dt_start * u2 * vel_mag
+        u3_dot = u4
+        u4_dot = - 2 * u2 * u4 / u1 - dt_start * u4 * vel_mag
+
+        return np.array([u1_dot, u2_dot, u3_dot, u4_dot])
+
+class SphericalAccelerations(PolarAccelerations):
+
+    @staticmethod
+    def comb_velocity(rad, radVel, polAng, polVel, aziVel):
+        return np.sqrt(radVel**2 + rad**2 * (polVel**2 + np.sin(polAng)**2 * aziVel**2))
+
+    # Spherical Acceleration functions to be passed into RK45
+    @staticmethod
+    def accelerations(u1, u2, u3, u4, u5, u6):
+        '''
+        :param u1: Radial Position
+        :param u2: Radial Velocity
+        :param u3: Polar Angular Position
+        :param u4: Polar Angular Velocity
+        :param u5: Azimuthal Angular Position
+        :param u6: Azimuthal Angular Velocity
+
+        :return: [Radial Velocity, Radial Acceleration, Polar Angular Velocity, Polar Angular Acceleration, Azimuthal Angular Velocity, Azimuthal Angular Acceleration]
+        '''
+
+        dt_start = SphericalAccelerations.drag_start(C_d=InitialConditions.dragCoeff,
+                                                 A=InitialConditions.crossSec,
+                                                 m=InitialConditions.satMass)
+
+        vel_mag = SphericalAccelerations.comb_velocity(u1, u2, u3, u4, u6)
+
+        u1_dot = u2
+        u2_dot = u1 * (u4**2 + np.sin(u3)**2 * u6**2) - InitialConditions.gravConstant * InitialConditions.earthMass / u1**2 - dt_start * u2 * vel_mag
+        u3_dot = u4
+        u4_dot = - 2 * u2 * u4 / u1 + np.sin(u3) * np.cos(u3) * u6**2 - dt_start * u4 * vel_mag
+        u5_dot = u6
+        u6_dot = - 2 * u2 * u6 / u1 - 2 * np.cot(u3) * u4 * u6 - dt_start * u6 * vel_mag
+
+        return np.array([u1_dot, u2_dot, u3_dot, u4_dot, u5_dot, u6_dot])
+
