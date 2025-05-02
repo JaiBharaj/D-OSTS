79ec18559a6f544cb1984a8cbdbae96682859dd5
tommmmiller <97626245+tommmmiller@users.noreply.github.com>
2025-05-02 11:48:04 +0100

    Add files via upload

---
+++ Atmospheric_Density.py
@@ -0,0 +1,12 @@
+import ussa1976
+import numpy as np
+ds = ussa1976.compute()
+
+def atmos_ussa1976_rho(altitude, ds):
+    # Find the closest index to altitude in metres
+    idx = np.argmin(np.abs(ds["z"].values - altitude))
+
+    # Return pressure, temperature, and density
+    return ds["rho"].values[idx]
+
+print(atmos_ussa1976_rho(1000, ds))
