785d4e1f43431a45179b72056702af3857003f2f
Jai Bharaj <jaibharaj@hotmail.com>
2025-05-04 22:42:39 +0100

    updating baseline

--- BaselineSolution.py
+++ BaselineSolution.py
@@ -14,5 +14,14 @@ NumericalIntegrator.Integrator.get_trajectory()
 # ---- ---- ---- ---- ---- ---- ---- ----
 #   ---- ---- ---- ---- ---- ---- ---- ---
 
-########## GENERATING FAKE RADAR DATA FOR TESTING ##########
-###### DELETE AFTER REAL RADAR CODE IS INSERTED ABOVE ######
+################# GENERATING FAKE RADAR DATA FOR TESTING #####################
+############## DELETE AFTER REAL RADAR CODE IS INSERTED ABOVE ################
+input_path = "trajectory_without_noise.txt"                                 ##
+output_path = "fake_radar_data.txt"                                         ##
+with open(input_path, 'r') as infile, open(output_path, 'w') as outfile:    ##
+    for i, line in enumerate(infile):                                       ##
+        if i % 100 == 0:                                                    ##
+            outfile.write(line)                                             ##
+##############################################################################
+
+########## TRAJECTORY PREDICTIONS WITH EXTENDED KALMAN FILTER ##########
