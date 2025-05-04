6387a83ec6de6ffa7248415cf5dc12b78d294f13
Jai Bharaj <jaibharaj@hotmail.com>
2025-05-04 21:04:11 +0100

    Init conditions

---
+++ CrudeInitialConditions.py
@@ -0,0 +1,9 @@
+class InitialConditions:
+
+    dragCoeff = 2
+    crossSec = 10
+    satMass = 50
+    initSatAlt = 400000
+    earthMass = 6E+24
+    earthRadius = 6.37E+6
+    gravConstant = 6.67E-11
