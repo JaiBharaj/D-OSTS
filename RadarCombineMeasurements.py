aa1e6fcccad4e97fa2791e06efd02e9269eee125
Jai Bharaj <jaibharaj@hotmail.com>
2025-05-05 20:17:18 +0100

    Improved NaN hangling

--- RadarCombineMeasurements.py
+++ RadarCombineMeasurements.py
@@ -4,6 +4,7 @@ def weighted_average(x, noise):
     weights = 1 / noise ** 2
     return np.sum(weights * x) / np.sum(weights)
 
+
 def combine_radar_measurements(radars, true_traj):
     times = true_traj[:, 0]
     theta_arr = true_traj[:, 2]
@@ -13,18 +14,20 @@ def combine_radar_measurements(radars, true_traj):
         seen_radius = []
         noise = []
         for rad in radars:
-            is_visible = rad.satellite_measurements['visibility'][i].copy()
+            is_visible = rad.satellite_measurements['visibility'][i]
             if not is_visible:
                 continue
-            seen_radius.append(rad.satellite_measurements['r'][i].copy())
-            noise.append(rad.get_noise()[i].copy())
-        seen_radius = np.array(seen_radius)
-        noise = np.array(noise)
-        average_radius = weighted_average(seen_radius, noise)
-        r_arr[i] = average_radius
+            seen_radius.append(rad.satellite_measurements['r'][i])
+            noise.append(rad.get_noise()[i])
+
+        if len(seen_radius) == 0:
+            r_arr[i] = np.nan  # <-- key fix: no visible radars
+        else:
+            seen_radius = np.array(seen_radius)
+            noise = np.array(noise)
+            r_arr[i] = weighted_average(seen_radius, noise)
 
     return np.array([times, r_arr, theta_arr]).T
-
 
 def write_to_file(file_name, traj):
     with open(file_name, 'w') as file:
