207663936d6e43126e2fc5b437139a53b9fd40f9
tommmmiller <97626245+tommmmiller@users.noreply.github.com>
2025-05-05 12:41:14 +0100

    Update BaselineSolution.py
    Slight Class alteration

--- BaselineSolution.py
+++ BaselineSolution.py
@@ -6,24 +6,10 @@ from CoordinateTransformations import PolarAccelerations
 from NumericalIntegrator import Integrator
 from ExtendedKalmanFilters import ExtendedKalmanFilter, compute_F_analytic
 from Visualiser import Visualiser2D
-from IntegratorWrapper import IntegratorWrapper
+from PredictorIntegrator import RK45Integrator
 from scipy.integrate import solve_ivp
 from scipy.linalg import expm
 
-
-class RK45Integrator:
-    def step(self, f, x, dt):
-        sol = solve_ivp(lambda t,y: f(y),
-                        (0, dt), x,
-                        rtol=1e-6, atol=1e-9,
-                        max_step=dt/10)
-        return sol.y[:, -1]
-
-    def transition_matrix(self, x, dt):
-        # continuous Jacobian at x (n×n)
-        F_cont = compute_F_analytic(x, CD, A, m, GM, atmos_ussa1976_rho)
-        # discrete transition matrix via matrix exponential
-        return expm(F_cont * dt)
 
 ########## GENERATING TRUE TRAJECTORY ##########
 Integrator.initialize(mu_value=InitialConditions.gravConstant * InitialConditions.earthMass)
