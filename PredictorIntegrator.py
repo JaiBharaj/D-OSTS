049175807362f590e791740494219d021d7c6223
tommmmiller <97626245+tommmmiller@users.noreply.github.com>
2025-05-07 11:49:21 +0100

    Update PredictorIntegrator.py
    Including 3D Transition Matrix

--- PredictorIntegrator.py
+++ PredictorIntegrator.py
@@ -1,6 +1,6 @@
 from scipy.integrate import solve_ivp
 from scipy.linalg import expm
-from ExtendedKalmanFilters import compute_F_analytic
+from ExtendedKalmanFilters import compute_F_analytic, compute_F_spherical
 
 class RK45Integrator:
     def __init__(self, CD, A, m, GM, rho_func):
@@ -20,3 +20,23 @@ class RK45Integrator:
     def transition_matrix(self, x, dt):
         F_cont = compute_F_analytic(x, self.CD, self.A, self.m, self.GM, self.rho_func)
         return expm(F_cont * dt)
+
+class RK45Integrator_3D:
+    def __init__(self, CD, A, m, GM, rho_func):
+        self.CD = CD
+        self.A = A
+        self.m = m
+        self.GM = GM
+        self.rho_func = rho_func
+
+    def step(self, f, x, dt):
+        sol = solve_ivp(lambda t, y: f(y),
+                        (0, dt), x,
+                        rtol=1e-6, atol=1e-9,
+                        max_step=dt/10)
+        return sol.y[:, -1]
+
+    def transition_matrix(self, x, dt):
+        F_cont = compute_F_spherical(x, self.CD, self.A, self.m, self.GM, self.rho_func)
+        return expm(F_cont * dt)
+
