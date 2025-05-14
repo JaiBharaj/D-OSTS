910be068b77bc8bb4b4cbda827e43ecc6da400df
daduke213 <david.dukov@warwick.ac.uk>
2025-05-14 22:31:20 +0100

    Update WriteToFiles.py

--- dosts/WriteToFiles.py
+++ dosts/WriteToFiles.py
@@ -12,7 +12,7 @@ def write_to_file_3d(file_name, traj):
             file.write(f"{t} {r} {theta} {phi}\n")
     print(f"Wrote {traj.shape[0]} points to {file_name}")
 
-# function to write 3D trajectory to file
+# function to write 3D trajectory to file inlcuding uncertainties
 def write_to_file_3d_vis(file_name, traj):
     with open(file_name, 'w') as file:
         for (t, r, theta, phi, s_r, s_theta, s_phi, meas) in zip(traj[:, 0], traj[:, 1], traj[:, 2], traj[:, 3], traj[:, 4], traj[:, 5], traj[:, 6], traj[:, 7]):
