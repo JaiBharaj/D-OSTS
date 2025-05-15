a57a719fbbb90a7d60fd54848fe7c318abee028b
Jai Bharaj <jaibharaj@hotmail.com>
2025-05-15 01:59:04 +0100

    generate new traj

--- Demo/Extension/ExtensionSimulator.py
+++ Demo/Extension/ExtensionSimulator.py
@@ -22,9 +22,16 @@ def run_simulator(mode, recorded_times=None):
     H_dark = 200000  # Possible for no radars to see satellite below this height
     distribute_radars = getattr(RadarModule, f"distribute_radars{mode.upper()}")
     radar_positions = distribute_radars(H_dark, IC.earthRadius)
+    radar_angle = np.pi / 2
+    radar_noise_base = 50  # meters
+    radar_noise_scalefactor = 0.0005  # m/km
 
     # Initialise radar stations
-    radars = RadarModule.initialise_radar_stations(mode, radar_positions)
+    radars = RadarModule.initialise_radar_stations(mode,
+                                                   radar_positions,
+                                                   radar_angle,
+                                                   radar_noise_base,
+                                                   radar_noise_scalefactor)
 
     # Record satellite positions in each radar
     for measurement in true_traj:
